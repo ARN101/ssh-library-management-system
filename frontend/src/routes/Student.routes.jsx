0eecb1a9c1c2153e306163e4a1d38ebe7c47db19
suaib022 <suaib.109206@gmail.com>
2026-07-02 19:36:00 +0600

    feat(SSH-7): build student book catalog page with search and category filter

---
+++ frontend/src/routes/Student.routes.jsx
@@ -0,0 +1,9 @@
+import BookCatalog from "../pages/BookCatalog.jsx";
+
+export const studentPaths = [
+  {
+    name: "Book Catalog",
+    path: "book-catalog",
+    element: <BookCatalog />,
+  },
+];
